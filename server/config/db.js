c3e0438979d0c802006284f323695b00666c0ef4
V.Mohanapavnkumar <mohanpavankumarvankayala@gmail.com>
2025-01-29 13:39:24 -0600

    TimeOut Updated

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI)
+        await mongoose.connect(process.env.MONGODB_URI, {
+            connectTimeoutMS: 30000, // 30 seconds
+            serverSelectionTimeoutMS: 30000
+          });
     }catch(err){
         console.log(err)
     }
