468e313f8617fcd28ba00f64267f4c97253c1be8
V.Mohanapavnkumar <mohanpavankumarvankayala@gmail.com>
2025-02-06 22:59:56 -0600

    Changes

--- client/src/components/UploadProduct.js
+++ client/src/components/UploadProduct.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { CgClose } from ''
 
 const UploadProduct = () => {
   return (
     <div className='fixed w-full h-full bg-slate-200 bg-opacity-35 top-0 left-0 right-0 bottom-0 flex justify-center items-center'>
-      <div className='bg-white p-4 rounded '>
-        UploadProduct
+      <div className='bg-white p-4 rounded w-full max-w-2xl h-full max-h-[80%]'>
+        <div>
+          <h2 className='font-bold text-lg'>UploadProduct</h2>
+        </div>
       </div>
     </div>
   )
