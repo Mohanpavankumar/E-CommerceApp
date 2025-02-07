160e750569d0d30402743acab50a4ef66612436d
V.Mohanapavnkumar <mohanpavankumarvankayala@gmail.com>
2025-02-06 22:49:14 -0600

    Changes

--- client/src/components/UploadProduct.js
+++ client/src/components/UploadProduct.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const UploadProduct = () => {
   return (
-    <div className=''>
+    <div className='fixed w-full h-full bg-slate-200 bg-opacity-35 top-0 left-0 right-0 bottom-0 flex justify-center items-center'>
+      <div className='bg-white p-4 rounded '>
         UploadProduct
+      </div>
     </div>
   )
 }
